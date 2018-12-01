13c329ce102e2ed9d18120d80abaf496af77fd12
JasmineSantinelli <44341927+JasmineSantinelli@users.noreply.github.com>
2018-12-01 22:15:26 +0100

    funzionante con anche latitude e longitude

--- app/src/main/java/com/example/jasmine/progettoinfo3/Upload.java
+++ app/src/main/java/com/example/jasmine/progettoinfo3/Upload.java
@@ -3,17 +3,12 @@ package com.example.jasmine.progettoinfo3;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import com.android.internal.http.multipart.MultipartEntity;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.FileEntity;
-import org.apache.http.entity.mime.FormBodyPart;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
@@ -25,15 +20,21 @@ public class Upload extends AsyncTask<String, Void, Void> {
     @Override
     protected Void doInBackground(String... params) {
         String responseString = null;
+        //assegnazione parametri
         String path=params[0];
-        Log.d("Log", "File path");
+        String longitudine=params[1];
+        String latitudine=params[2];
+
+       // Log.d("Log", "File path");
         HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost("http://progettoscandurra.andreacavagna.it/caricacellulareunsafe");
+        HttpPost httppost = new HttpPost("http://progettoscandurra.andreacavagna.it/caricacellulare");
 
         try {
             File file = new File(path);
             MultipartEntityBuilder entityBuilder=MultipartEntityBuilder.create();
             entityBuilder.addBinaryBody("file",file);
+            entityBuilder.addTextBody("long",longitudine);
+            entityBuilder.addTextBody("lat",latitudine);
             HttpEntity entity=entityBuilder.build();
             httppost.setEntity(entity);
 
@@ -42,18 +43,7 @@ public class Upload extends AsyncTask<String, Void, Void> {
             HttpEntity r_entity = response.getEntity();
             String result=EntityUtils.toString(r_entity);
             Log.v("results",result);
-            /**
-            int statusCode = response.getStatusLine().getStatusCode();
-            if (statusCode == 200) {
-                // Server response
-                responseString = EntityUtils.toString(r_entity);
-                Log.d("Log", responseString);
-            } else {
-                responseString = "Error occurred! Http Status Code: "
-                        + statusCode + " -> " + response.getStatusLine().getReasonPhrase();
-                Log.d("Log", responseString);
-            }
-**/
+
         } catch (ClientProtocolException e) {
             responseString = e.toString();
             Log.v("exception",responseString);
@@ -64,60 +54,9 @@ public class Upload extends AsyncTask<String, Void, Void> {
 
         return(null);
     }
-
+    protected void doPostExecute(){
+//bo, dovremo farci qualcosa con i risultati
+    }
 }
 
 
-
-
-
-
-
-
-
-/**protected void onCreate(Bundle savedInstanceState) {
- super.onCreate(savedInstanceState);
- setContentView(R.layout.activity_upload);
- //save path taken from Main_Activity
- String path=null;
- path=getIntent().getExtras().getString("path");
- String responseString = null;
- Log.d("Log", "File path");
- HttpClient httpclient = new DefaultHttpClient();
- HttpPost httppost = new HttpPost("http://progettoscandurra.andreacavagna.it/caricacellulare");
- try {
- // MultipartEntity entity = new MultipartEntity();
- //ExifInterface newIntef = new ExifInterface(path);
- //newIntef.setAttribute(ExifInterface.TAG_ORIENTATION,String.valueOf(2));
- File file = new File(path);
- FormBodyPart part=new FormBodyPart("pic",new FileBody(file));
- //entity.addPart(part);
- FileEntity entity=new FileEntity(file,"pic");
- //long totalSize = entity.getContentLength();
- httppost.setEntity(entity);
- // Making server call
- HttpResponse response = httpclient.execute(httppost);
- HttpEntity r_entity = response.getEntity();
- int statusCode = response.getStatusLine().getStatusCode();
- if (statusCode == 200) {
- // Server response
- responseString = EntityUtils.toString(r_entity);
- Log.d("Log", responseString);
- } else {
- responseString = "Error occurred! Http Status Code: "
- + statusCode + " -> " + response.getStatusLine().getReasonPhrase();
- Log.d("Log", responseString);
- }
- } catch (ClientProtocolException e) {
- responseString = e.toString();
- } catch (IOException e) {
- responseString = e.toString();
- }
- Toast.makeText(Upload.this, "Risposta" + responseString, Toast.LENGTH_SHORT).show();
- //return responseString;
- }
- public void back(View view) {
- Intent intent = new Intent(Upload.this, MainActivity.class);
- startActivity(intent);
- }
- }*/
