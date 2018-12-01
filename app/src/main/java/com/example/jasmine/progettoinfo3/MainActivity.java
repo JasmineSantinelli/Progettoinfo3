72af5d4e129b7683b77756448a2157718c0d3100
JasmineSantinelli <44341927+JasmineSantinelli@users.noreply.github.com>
2018-12-01 13:30:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jasmine/progettoinfo3/MainActivity.java
@@ -0,0 +1,217 @@
+package com.example.jasmine.progettoinfo3;
+import android.Manifest;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.List;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
+public class MainActivity extends AppCompatActivity {
+    //Declaration
+    private String path=null;
+    private Button btnUpload;
+    private Button btn;
+    private ImageView imageview;
+    private static final String IMAGE_DIRECTORY = "/PROGETTOINFO3";
+    private int GALLERY = 1, CAMERA = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestMultiplePermissions();
+        btn = (Button) findViewById(R.id.btn);
+        btnUpload= (Button) findViewById(R.id.btn_upload);
+        imageview = (ImageView) findViewById(R.id.iv);
+        //SE PREMO IL BTN RICHIAMA IL METODO PER APRIRE LA FINESTRA DI DIALOGO
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPictureDialog();
+            }
+        });
+
+    }
+
+    //select between camera or gallery to upload an image
+    private void showPictureDialog(){
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+        pictureDialog.setTitle("Cosa vuoi fare?");
+        String[] pictureDialogItems = {
+                "Seleziona una foto dalla galleria",
+                "Scatta una foto" };
+        pictureDialog.setItems(pictureDialogItems,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                choosePhotoFromGallery();
+                                break;
+                            case 1:
+                                takePhotoFromCamera();
+                                break;
+                        }
+                    }
+                });
+        pictureDialog.show();
+    }
+
+    //select a photo from gallery
+    public void choosePhotoFromGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(galleryIntent, GALLERY);
+    }
+
+    //capture a picture
+    private void takePhotoFromCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA);
+    }
+
+    //Activity results
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == this.RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == GALLERY) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                    path = saveImage(bitmap);
+                    Toast.makeText(MainActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+                    imageview.setImageBitmap(bitmap);
+                    btnUpload.setVisibility(View.VISIBLE);
+                    btn.setText("Modifica inserimento");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        } else if (requestCode == CAMERA) {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            imageview.setImageBitmap(thumbnail);
+            btnUpload.setVisibility(View.VISIBLE);
+            btn.setText("Modifica inserimento");
+            path=saveImage(thumbnail);
+            Toast.makeText(MainActivity.this, "Image Saved!"+path, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //save image in specified directory
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File(
+                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
+        // have the object build the directory structure, if needed.
+        if (!wallpaperDirectory.exists()) {
+            wallpaperDirectory.mkdirs();
+        }
+
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(this,
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::---&gt;" + f.getAbsolutePath());
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+    }
+    //require permission
+    private void  requestMultiplePermissions(){
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        // check if all permissions are granted
+                        if (report.areAllPermissionsGranted()) {
+                            Toast.makeText(getApplicationContext(), "All permissions are granted by user!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // check for permanent denial of any permission
+                        /**if (report.isAnyPermissionPermanentlyDenied()) {
+                         // show alert dialog navigating to Settings
+                         //openSettingsDialog();
+                         }**/
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+
+                })
+                .withErrorListener(new PermissionRequestErrorListener() {
+                    @Override
+                    public void onError(DexterError error) {
+                        Toast.makeText(getApplicationContext(), "Some Error! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .onSameThread()
+                .check();
+    }
+
+    //change Activity
+    public void upload(View view) {
+        new Upload().execute(path);
+    }
+
+}
+
+
+
---
+++ app/src/main/java/com/example/jasmine/progettoinfo3/Upload.java
@@ -0,0 +1,131 @@
+package com.example.jasmine.progettoinfo3;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.entity.mime.FormBodyPart;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Upload extends AsyncTask<String, Void, Void> {
+
+    @Override
+    protected Void doInBackground(String... params) {
+        String responseString = null;
+        String path=params[0];
+        Log.d("Log", "File path");
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://progettoscandurra.andreacavagna.it/caricacellulare");
+
+        try {
+            // MultipartEntity entity = new MultipartEntity();
+            //ExifInterface newIntef = new ExifInterface(path);
+            //newIntef.setAttribute(ExifInterface.TAG_ORIENTATION,String.valueOf(2));
+
+            File file = new File(path);
+            FormBodyPart part=new FormBodyPart("pic",new FileBody(file));
+            //entity.addPart(part);
+            FileEntity entity=new FileEntity(file,"pic");
+            //long totalSize = entity.getContentLength();
+            httppost.setEntity(entity);
+
+            // Making server call
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity r_entity = response.getEntity();
+
+
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode == 200) {
+                // Server response
+                responseString = EntityUtils.toString(r_entity);
+                Log.d("Log", responseString);
+            } else {
+                responseString = "Error occurred! Http Status Code: "
+                        + statusCode + " -> " + response.getStatusLine().getReasonPhrase();
+                Log.d("Log", responseString);
+            }
+
+        } catch (ClientProtocolException e) {
+            responseString = e.toString();
+        } catch (IOException e) {
+            responseString = e.toString();
+        }
+
+        return(null);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+/**protected void onCreate(Bundle savedInstanceState) {
+ super.onCreate(savedInstanceState);
+ setContentView(R.layout.activity_upload);
+ //save path taken from Main_Activity
+ String path=null;
+ path=getIntent().getExtras().getString("path");
+ String responseString = null;
+
+ Log.d("Log", "File path");
+ HttpClient httpclient = new DefaultHttpClient();
+ HttpPost httppost = new HttpPost("http://progettoscandurra.andreacavagna.it/caricacellulare");
+
+ try {
+ // MultipartEntity entity = new MultipartEntity();
+ //ExifInterface newIntef = new ExifInterface(path);
+ //newIntef.setAttribute(ExifInterface.TAG_ORIENTATION,String.valueOf(2));
+
+ File file = new File(path);
+ FormBodyPart part=new FormBodyPart("pic",new FileBody(file));
+ //entity.addPart(part);
+ FileEntity entity=new FileEntity(file,"pic");
+ //long totalSize = entity.getContentLength();
+ httppost.setEntity(entity);
+
+ // Making server call
+ HttpResponse response = httpclient.execute(httppost);
+ HttpEntity r_entity = response.getEntity();
+
+
+ int statusCode = response.getStatusLine().getStatusCode();
+ if (statusCode == 200) {
+ // Server response
+ responseString = EntityUtils.toString(r_entity);
+ Log.d("Log", responseString);
+ } else {
+ responseString = "Error occurred! Http Status Code: "
+ + statusCode + " -> " + response.getStatusLine().getReasonPhrase();
+ Log.d("Log", responseString);
+ }
+
+ } catch (ClientProtocolException e) {
+ responseString = e.toString();
+ } catch (IOException e) {
+ responseString = e.toString();
+ }
+ Toast.makeText(Upload.this, "Risposta" + responseString, Toast.LENGTH_SHORT).show();
+ //return responseString;
+ }
+
+ public void back(View view) {
+ Intent intent = new Intent(Upload.this, MainActivity.class);
+ startActivity(intent);
+
+ }
+ }*/
