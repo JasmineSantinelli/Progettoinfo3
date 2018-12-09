f21a13bfd7e6df94ca57750d78ba6c1c0eb5e907
AndreaCavagna <andrea.cavagna.94@gmail.com>
2018-12-09 21:39:15 +0100

    aggiunto menu laterale, se funziona

--- app/src/main/java/com/example/jasmine/progettoinfo3/MainActivity.java
+++ app/src/main/java/com/example/jasmine/progettoinfo3/MainActivity.java
@@ -19,11 +19,15 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v4.content.FileProvider;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -97,7 +101,7 @@ public class MainActivity extends AppCompatActivity  implements AsyncResponse {
     private EditText name;
     private final String DefaultUnameValue = "";
     private String UnameValue;
-
+    private DrawerLayout mDrawerLayout;
     private FusedLocationProviderClient mFusedLocationClient;
 
     private LocationManager mLocationManager;
@@ -174,12 +178,59 @@ public class MainActivity extends AppCompatActivity  implements AsyncResponse {
         text3 = findViewById(R.id.textView7);
         text4 = findViewById(R.id.textView8);
         name = findViewById(R.id.editText);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_add_white);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+
         FloatingActionButton floatButton = findViewById(R.id.floatingActionButton3);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        // Respond when the drawer is opened
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }
+                }
+        );
+
+
+
 
         LocationRequest locationRequest = LocationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
@@ -251,6 +302,16 @@ public class MainActivity extends AppCompatActivity  implements AsyncResponse {
         aggiornaPosix();
         aggiornaPosix2();
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
